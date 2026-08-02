81eb2df32307b3a8deb4dd4ec3eabcf42488a917
Alex <matrixn@users.noreply.github.com>
2026-08-02 18:22:38 +0300

    Testing plugin deployment

--- plugin.php
+++ plugin.php
@@ -6,6 +6,18 @@ Author: Your Name
 Version: 1.1.1
 */
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Zion\WordPressLicense\Config;
+use Zion\WordPressLicense\LicenseManager;
+
+$licenseManager = new LicenseManager(new Config(
+    apiUrl: 'https://license.zion3d.ro/api/v1',
+    productSlug: 'my-new-plugin-demo',
+    productKey: 'zion_uwmrn2stfd1o5w4jk2dse2qdiokgkzf7nhnacp7h',
+    pluginFile: __FILE__,
+));
+
 function hello_world_display() {
   echo "Hello World using a template changed by Vikas 17 aug Push 1";
 }
